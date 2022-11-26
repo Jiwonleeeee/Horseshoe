074c22b6256a586a7fca84b1b59f54a43b32b87d
Jiwon Lee <lee9j2@mail.uc.edu>
2022-11-26 17:10:10 -0500

    add plot, last check

--- RejectSampler_check.R
+++ RejectSampler_check.R
@@ -57,16 +57,42 @@ eta_update_ftn <- function(eta_j_input, k_input){
   u <- runif(1)
   eta_j_output <- ifelse(u < exp(-f_x(eta_j_q)+f_L_x(eta_j_q, case)), eta_j_q, eta_j_input)
   check <- ifelse(exp(-f_L_x(eta_j_q, case))>=exp(-f_x(eta_j_q)),1,0)
+  # print(check)
   acc_prob <- exp(-f_x(eta_j_q)+f_L_x(eta_j_q, case))
-  return(acc_prob)
+  return(eta_j_output)
 }
 
-eta_update_ftn(0.5,1)
 
-xx <- seq(0,100,0.01)
-l <- length(xx)
-check_v <- numeric(i)
-for(i in 1:l){
-  check_v[i] <- eta_update_ftn(xx[i],1)
+k <- sapply(true_beta, function(x_input){(x_input^2 * 1) / (2 * true_sigma2)})+0.0001
+
+
+### ex. beta= 2,3,4
+y <- function(b_input,x_input){
+  
+  exp(-b_input*x_input)/(1+x_input)
+  
 }
-hist(check_v)
+
+x <- seq(0,5,0.01)
+
+plot(x,y(2,x),type="l", ylab="eta",main="eta target function depeding on beta=2,3,4")
+lines(x,y(3,x),col="red")
+lines(x,y(4,x),col="blue")
+legend(4,0.8, legend=c("2","3","4"), col=c("black", "red","blue"), lwd=1, cex=0.8)
+
+
+k <- sapply(true_beta, function(x_input){(x_input^2 * 1) / (2 * true_sigma2)})+0.0001
+
+## check
+rep <- 10^4
+eta_matrix <- matrix(0, nrow=rep, ncol=p)
+for(j in 1:p){
+  eta_matrix[1,j] <- 0.5
+  for(i in 2:rep){
+    eta_matrix[i,j] <- eta_update_ftn(eta_matrix[i-1,j],k[j])
+  }
+  print(j)
+}
+plot(apply(eta_matrix,2,mean), ylab="eta",main="eta check")
+points(1:p, true_beta, col="red",cex=0.3)
+legend(0, 2000, legend=c("estimated eta", "true beta"), col=c("black","red"),pch=1,cex=0.7)
