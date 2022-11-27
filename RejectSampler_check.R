d3e7d7e60e92c909f79295b5b366f655567ce529
Jiwon Lee <lee9j2@mail.uc.edu>
2022-11-27 00:54:12 -0500

    weight check

--- RejectSampler_check.R
+++ RejectSampler_check.R
@@ -38,7 +38,7 @@ eta_update_ftn <- function(eta_j_input, k_input){
   p2 <- v2/v
   p3 <- v3/v
   p4 <- v4/v
-  
+  prob <- c(p1,p2,p3,p4)
   # print(c(p1,p2,p3,p4))
   
   H2 <- 1-exp(-B+A)
@@ -47,7 +47,6 @@ eta_update_ftn <- function(eta_j_input, k_input){
   u <- runif(1)
   
   case <- sample(c(1:4), 1, prob=c(p1,p2,p3,p4))
-  
   eta_j_q <- switch(case,
                     (1+a)^u -1,
                     a - log(1-u*H2)/lambda2,
@@ -59,7 +58,8 @@ eta_update_ftn <- function(eta_j_input, k_input){
   check <- ifelse(exp(-f_L_x(eta_j_q, case))>=exp(-f_x(eta_j_q)),1,0)
   # print(check)
   acc_prob <- exp(-f_x(eta_j_q)+f_L_x(eta_j_q, case))
-  return(eta_j_output)
+  return(list(eta_j_output=eta_j_output, prob=prob))
+
 }
 
 
@@ -75,10 +75,15 @@ y <- function(b_input,x_input){
 
 x <- seq(0,5,0.01)
 
-plot(x,y(2,x),type="l", ylab="eta",main="eta target function depeding on beta=2,3,4")
+plot(x,y(2,x),type="l", ylab="eta",main="eta target function depending on beta")
 lines(x,y(3,x),col="red")
 lines(x,y(4,x),col="blue")
 legend(4,0.8, legend=c("2","3","4"), col=c("black", "red","blue"), lwd=1, cex=0.8)
+
+
+plot(x,y(2,x),type="l", ylab="f(x)",main=expression(f(x)==frac(e^-{2*x},1+x)), ylim=c(0,2),xlim=c(0,2))
+lines(x,3*y(2,x),col="red")
+legend(1.2,1.5, legend=c("f(x)","2f(x)"), col=c("black", "red"), lwd=1, cex=0.8)
 
 
 k <- sapply(true_beta, function(x_input){(x_input^2 * 1) / (2 * true_sigma2)})+0.0001
@@ -96,3 +101,19 @@ for(j in 1:p){
 plot(apply(eta_matrix,2,mean), ylab="eta",main="eta check")
 points(1:p, true_beta, col="red",cex=0.3)
 legend(0, 2000, legend=c("estimated eta", "true beta"), col=c("black","red"),pch=1,cex=0.7)
+
+# weight check
+wgt_mat <- matrix(0, rep, 4)
+eta_temp <- numeric(rep)
+eta_temp[1] <- 0.5
+for(i in 2:rep){
+  eta_temp[i] <- eta_update_ftn(eta_temp[i-1],k[1])$eta_j_output
+  wgt_mat[i,] <- eta_update_ftn(eta_temp[i-1],k[1])$prob
+}
+
+apply(wgt_mat,2,mean)
+
+# negative log
+xx <- seq(0,5,0.01)
+yy <- log(xx+0.5)
+plot(xx,yy,type="l",xlab="x",ylab="h(x)")
